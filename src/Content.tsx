05e0bb08577a163677b9254aae94172ea3f413e6
luizpaulo73 <luiz.paulo2005@hotmail.com>
2025-08-17 19:31:53 -0300

    refactor: update imports

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import { ActivityIndicator, FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { getUsers } from "./api/posts";
 import ErrorComponent from "./components/Error/Error";
-import PostStructure from "./components/PostStructure/PostStructure";
+import Loading from "./components/Loading/Loading";
+import UserList from "./components/UserList/UserList";
 
 export default function Content() {
     const { data, isLoading, error, isFetching, refetch } = useQuery({
@@ -11,74 +11,11 @@ export default function Content() {
         queryFn: getUsers,
     });
 
-    if (isLoading) return <ActivityIndicator size={"large"} />;
+    if (isLoading) return <Loading />;
 
     if (error) {
-        return (
-            <ErrorComponent error={error} />
-        );
+        return <ErrorComponent error={error} />;
     }
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.headerContainer}>
-                <Text style={styles.headerTitle}>User List</Text>
-                <View style={styles.headerUnderline} />
-            </View>
-
-            <FlatList
-                style={styles.flatList}
-                contentContainerStyle={styles.flatListContent}
-                data={data}
-                refreshing={isFetching}
-                onRefresh={refetch}
-                renderItem={({ item }) => <PostStructure item={item} />}
-                showsVerticalScrollIndicator={false}
-            />
-        </SafeAreaView>
-    );
+    return <UserList data={data} isFetching={isFetching} refetch={refetch} />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#f5f7fa",
-    },
-    headerContainer: {
-        alignItems: "center",
-        paddingTop: 30,
-        paddingBottom: 16,
-        paddingHorizontal: 16,
-        backgroundColor: "#ffffff",
-        shadowColor: "#4a69bd",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.08,
-        shadowRadius: 4,
-        elevation: 3,
-        marginBottom: 8,
-    },
-    headerTitle: {
-        fontSize: 22,
-        fontWeight: "800",
-        color: "#4a69bd",
-        letterSpacing: 1.2,
-        textAlign: "center",
-    },
-    headerUnderline: {
-        width: 80,
-        height: 4,
-        backgroundColor: "#4a69bd",
-        borderRadius: 2,
-        marginTop: 8,
-    },
-    flatList: {
-        flex: 1,
-    },
-    flatListContent: {
-        paddingVertical: 8,
-        paddingBottom: 20,
-    },
-});
